2022c26b0397ce9457b2cf7ae2dc5f276a5c3e30
Christian Hopper <hopperchristian@yahoo.com>
2025-05-05 15:31:57 -0700

    Add smooth transitions for modal opening/closing. Add persistence to the text when editing the profile. Add console log functionality to display the image link and caption inputs to the console when creating a new post.

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,29 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
 const newPostButton = document.querySelector(".profile__post-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#image-link-input");
+const newPostCaptionInput = newPostModal.querySelector("#image-caption-input");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
 
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-open");
 });
 
@@ -22,3 +40,22 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-open");
 });
+
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-open");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+  console.log("Image link", newPostImageInput.value);
+  console.log("Image caption", newPostCaptionInput.value);
+  newPostModal.classList.remove("modal_is-open");
+  newPostForm.reset();
+}
+
+newPostForm.addEventListener("submit", handleNewPostFormSubmit);
